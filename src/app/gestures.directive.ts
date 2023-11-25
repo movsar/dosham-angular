2797dd99f224dc8e894f107700c8264d617052b3
= <movsar.dev@gmail.com>
2023-11-25 18:05:47 +0300

    fixed scrolling

--- src/app/gestures.directive.ts
+++ src/app/gestures.directive.ts
@@ -1,30 +1,44 @@
-import { Directive, ElementRef, Output, EventEmitter, HostListener } from '@angular/core';
+import { Directive, ElementRef, Output, EventEmitter, OnInit, OnDestroy } from '@angular/core';
 import * as Hammer from 'hammerjs';
 
 @Directive({
   selector: '[appGestures]'
 })
-export class GesturesDirective {
+export class GesturesDirective implements OnInit, OnDestroy {
 
   @Output() topDownSwipe = new EventEmitter<void>();
   @Output() leftToRightSwipe = new EventEmitter<void>();
+  private hammerManager?: HammerManager;
 
   constructor(private el: ElementRef) { }
 
-  @HostListener('swipe', ['$event'])
-  onSwipe(event: any) {
-    event.preventDefault();
+  ngOnInit() {
+    this.hammerManager = new Hammer.Manager(this.el.nativeElement, {
+      touchAction: 'pan-y'
+    });
 
-    switch (event.direction) {
-      case Hammer.DIRECTION_RIGHT:
-        // Gets called twice per swipe
-        this.leftToRightSwipe.emit();
-        break;
+    this.hammerManager.add(new Hammer.Swipe({ direction: Hammer.DIRECTION_ALL }));
 
-      case Hammer.DIRECTION_DOWN:
-        this.topDownSwipe.emit();
-        break;
+    this.hammerManager.on('swipe', (event) => {
+      switch (event.direction) {
+        case Hammer.DIRECTION_RIGHT:
+          this.debouncedEmit(this.leftToRightSwipe);
+          break;
+        case Hammer.DIRECTION_DOWN:
+          this.debouncedEmit(this.topDownSwipe);
+          break;
+      }
+    });
+  }
 
+  ngOnDestroy() {
+    if (this.hammerManager) {
+      this.hammerManager.destroy();
     }
+  }
+
+  private debouncedEmit(emitter: EventEmitter<void>) {
+    // Implement debounce logic here
+    emitter.emit();
   }
 }
